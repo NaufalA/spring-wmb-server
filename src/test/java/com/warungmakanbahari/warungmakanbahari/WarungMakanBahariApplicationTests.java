ca93090f94f5ef4de6cf5edcb7adbe2bf8593917
NaufalAulia <naufal7600@gmail.com>
2023-01-10 09:14:21 +0700

    Initial Commit

---
+++ src/test/java/com/warungmakanbahari/warungmakanbahari/WarungMakanBahariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warungmakanbahari.warungmakanbahari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarungMakanBahariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
